20d13ea998825c0de2ff8929ea24c17e29a21fa7
Matthew <mateuszzaniewski1@gmail.com>
2023-10-27 21:16:11 +0200

    create: another basics tables in database: reviw and tag

--- projects/models.py
+++ projects/models.py
@@ -3,11 +3,38 @@ import uuid
 # Create your models here.
 class Project(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    tags = models.ManyToManyField('Tag', blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_linkg = models.CharField(max_length=2000, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    #owner = 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE) # jesli usune projekt to review tez bedzie usuniete
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=250)
+    
+    def __str__(self):
+        return self.name
     
